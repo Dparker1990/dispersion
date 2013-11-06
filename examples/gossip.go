a6379f457aa0436e73a884f8878f45d8463792f9
Derek Parker <parkerderek86@gmail.com>
2013-11-06 10:27:47 -0600

    Update gossip example

--- examples/gossip.go
+++ examples/gossip.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/Dparker1990/dispersion/gossip"
 	"os"
 	"time"
 )
 
 func blather(node *gossip.Node) {
-	node.Gossip()
-	time.Sleep(1 * time.Second)
+	for {
+		node.Gossip()
+		fmt.Println(fmt.Sprintf("Hash is now: %v", node.Peers))
+		time.Sleep(1 * time.Second)
+	}
 }
 
 func main() {
@@ -21,13 +25,10 @@ func main() {
 	config := gossip.Config{Bindip: "127.0.0.1", Bindport: "9292", Seeds: []string{s}}
 	node := gossip.NewNode(config)
 
-	if !seed {
+	if seed {
+		go node.StartServer()
+	} else {
 		node.Register()
 	}
-	go blather(node)
-	go node.StartServer()
-
-	for {
-		time.Sleep(1)
-	}
+	blather(node)
 }
