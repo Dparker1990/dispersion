8fdefa06cab945c974103904449f2376e826ec47
Derek Parker <parkerderek86@gmail.com>
2013-11-07 13:18:35 -0600

    Remove dead code

--- gossip/config.go
+++ gossip/config.go
@@ -1,28 +1,8 @@
 package gossip
 
-import (
-	"log"
-	"net"
-)
-
 type Config struct {
 	Bindip   string
-	boundip  net.IP
 	Bindport string
 	Seeds    []string
 	Logfile  string
-}
-
-func (c Config) BoundIP() (ip net.IP) {
-	if ip = c.boundip; ip != nil {
-		return
-	}
-
-	if ip = net.ParseIP(c.Bindip); ip == nil {
-		log.Fatalf("Could not parse IP, got: %v, original was: %v", ip, c.Bindip)
-	}
-
-	c.boundip = ip
-
-	return
 }
